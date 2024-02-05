1927195be4ee0efe8cf97a88f849d821dc3299d6
AgusBelP <aguspiuzzi@gmail.com>
2024-02-05 20:37:43 -0300

    Fin de sección 7 - Endpoints utilizando Mongo

--- models/index.js
+++
@@ -1,7 +0,0 @@
-const models = {
-    usersModel : require('../models/nosql/users.model'),
-    tracksModel : require('../models/nosql/tracks.model'),
-    storageModel : require('../models/nosql/storage.model')
-};
-
-module.exports = models
